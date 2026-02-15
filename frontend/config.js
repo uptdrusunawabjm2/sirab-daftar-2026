f9be5d72a1a3135e3ef155ce2ed3595a84f7f63e
uptdrusunawabjm2 <uptd.rusunawa.bjm2@gmail.com>
2026-02-15 19:32:53 +0800

    Update config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,5 @@
 const CONFIG = {
-  API: "https://script.google.com/macros/s/https://script.google.com/macros/s/AKfycbySr6QhyH72b4aAU6Xv6mltljm_ft07ysWb40J45QivoXYix8Xh9M1kJ8zLe4PzQ5i8/exec",
+  API: "https://script.google.com/macros/s/AKfycbySr6QhyH72b4aAU6Xv6mltljm_ft07ysWb40J45QivoXYix8Xh9M1kJ8zLe4PzQ5i8/exec",
   INTERNAL_KEY: "rusun2026"
 };
+
